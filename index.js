2e565d29b26946e391f25cc5e8231ce1383bc4c9
JoaoVitorm0nteiro <jv147pao@gmail.com>
2022-09-15 10:09:55 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require ('fs');
+
+const basepath = 'Ddiretorioteste/'
+const prefix = 'ytdown-'
+
+fs.readdir(basepath, (error, files) => {
+    files.map(file => {
+        if(file.startsWith(prefix)){
+            fs.rename(basepath + file, basepath + file.slice(prefix.length), (err)=>{
+                if(err){
+                    console.log('FALHA', err.message);
+                }else{
+                    console.log('Sucesso');
+                }
+            })
+        }
+    })
+})
